8716c9aea7fb9d95cf79462ccf6e34782e8c0dad
your_name <alex.martishyn@gmail.com>
2024-06-05 16:29:39 -0700

    install logrocket

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import LogRocket from 'logrocket';
+LogRocket.init('0eqphi/martik');
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
