6d20bd251a695124fd0e1535587869d109c677bd
Alex Martishyn <alex.martishyn@gmail.com>
2025-11-04 12:15:49 -0800

    SEO_09_fix

--- src/index.js
+++ src/index.js
@@ -1,44 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { HelmetProvider } from 'react-helmet-async';
-// import './index.css';
-// import LogRocket from 'logrocket';
-// LogRocket.init('0eqphi/martik');
+import App from './App';
 
-function ensureRoot() {
-  let el = document.getElementById('root');
-  if (!el) {
-    el = document.createElement('div');
-    el.id = 'root';
-    document.body.appendChild(el);
-  }
-  return el;
+
+import LogRocket from 'logrocket';
+if (process.env.NODE_ENV === 'production') {
+  LogRocket.init('0eqphi/martik');
 }
 
-function mount() {
-  const container = ensureRoot();
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-  if (!window.__APP_ROOT__) {
-    window.__APP_ROOT__ = ReactDOM.createRoot(container);
-  }
-
-  window.__APP_ROOT__.render(
-<React.StrictMode>
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
-</React.StrictMode>
-  );
-}
-
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', mount);
-} else {
-  mount();
-}
-
-// Только CRA/Webpack HMR
-if (typeof module !== 'undefined' && module.hot) {
-  module.hot.accept();
-}
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
